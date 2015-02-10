97a926c2f47e993eaccb1fc49cee2fd04a797a18
gouf <innocent.zero@gmail.com>
2015-02-10 16:01:58 +0900

    Validates image file name uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+class ImageNameUniquenessValidator < ActiveModel::Validator
+  def validate(record)
+    error_message = 'Image name is not uniqueness'
+    record.errors[:base] << error_message if uniqueness?(record)
+  end
+
+  private
+
+  def uniqueness?(record)
+    User.find_by_image(record.image.identifier)
+  end
+end
+
 class User < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  validates_with ImageNameUniquenessValidator
 end
